2337c914a0c2f5e0013b963213d3b02d507c0386
Ericlys Moreira <ericlysm@gmail.com>
2023-06-16 00:43:02 -0300

    autenticacao com clerk

---
+++ auth-clerk-server/src/private.ts
@@ -0,0 +1,25 @@
+import { clerkClient, clerkPlugin, getAuth } from "@clerk/fastify";
+import { FastifyInstance } from "fastify";
+
+export async function privateRoutes(app: FastifyInstance) {
+  app.register(clerkPlugin)
+
+  app.get('/private', async (request, reply) => {
+    const {userId} = getAuth(request)
+
+    if(!userId) {
+      return reply.status(403).send()
+    }
+
+    const user = await clerkClient.users.getUser(userId)
+    // const user = await clerkClient.users.updateUserMetadata(
+    //   userId, {
+    //     privateMetadata: {
+    //       role: 'ADMIN'
+    //     }
+    //   }
+    // )
+
+    return  user
+  })
+}
